55fecc2824440a967f2574717c10c67f5222a461
adamlui@protonmail.com <adamlui@protonmail.com>
2023-04-07 14:23:48 -0700

    Changed `background.js.matches` to auto-extract from manifest

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -1,7 +1,11 @@
 // Monitor active tab URL to toggle extension on matches
 
-var matches = [ 'chat.openai.com', 'freegpt.one' ]
+// Extract content script matches from manifest
+var matches = chrome.runtime.getManifest().content_scripts.map(
+    (contentScript) => contentScript.matches.map(
+        (match) => match.replace(/\*$/, ''))).flat()
 
+// Add listener
 chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.tabs.get(activeInfo.tabId, (tab) => {
         if (matches.some((url) => tab.url.includes(url))) toggleExtension()
